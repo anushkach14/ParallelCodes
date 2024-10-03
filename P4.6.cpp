f4c8ec3692984af2ea647667b6d14a6b42fe629b
anushkach14 <129940327+anushkach14@users.noreply.github.com>
2024-10-03 07:59:23 +0530

    Subtraction of arrays

--- P4.6.cpp
+++ P4.6.cpp
@@ -1,26 +1,30 @@
-#include <stdio.h>
+##include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
+#include <time.h>
 
-#define ARRAY_SIZE 10  
+#define SIZE 1000
 
 int main() {
-    int array[ARRAY_SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
-    int squared[ARRAY_SIZE];
+    int vector_a[SIZE], vector_b[SIZE], result[SIZE];
+
+    srand(time(NULL));
+
+    // Initialize the vectors with random values between 0 and 100
+    for (int i = 0; i < SIZE; i++) {
+        vector_a[i] = rand() % 100; 
+        vector_b[i] = rand() % 100; 
+    }
 
     #pragma omp parallel for
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        squared[i] = array[i] * array[i];  
+    for (int i = 0; i < SIZE; i++) {
+        result[i] = vector_a[i] - vector_b[i];  
     }
-    
-    printf("Original Array:\n");
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%d ", array[i]);
+
+    printf("Vector A\tVector B\tResult\n");
+    for (int i = 0; i < SIZE; i++) {
+        printf("%d\t\t%d\t\t%d\n", vector_a[i], vector_b[i], result[i]);
     }
-    printf("\n\nSquared Array:\n");
-    for (int i = 0; i < ARRAY_SIZE; i++) {
-        printf("%d ", squared[i]);
-    }
-   
+
     return 0;
 }
-
