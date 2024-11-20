5a695d16429aab518d9f6d22dcce1b139318e6d7
anushkach14 <129940327+anushkach14@users.noreply.github.com>
2024-11-20 20:45:27 +0530

    Update P4.4.cpp

--- P4.4.cpp
+++ P4.4.cpp
@@ -3,41 +3,34 @@
 #include <omp.h>
 #include <time.h>
 
-#define SIZE 1000
+#define SIZE 1000  // Array size
 
 int main() {
-    int array[SIZE];
-    int max_val;
+    int arr[SIZE];
+    int i;
+    int max_value;
 
-    // Parallel region for generating random numbers
-    #pragma omp parallel
-    {
-        // Get the thread ID
-        int thread_id = omp_get_thread_num();
-        
-        // Set a unique seed for each thread using time and thread_id with an offset
-        unsigned int seed = time(NULL) + thread_id * 100;
-        srand(seed);
+    // Seed the random number generator
+    srand(time(NULL));
 
-        #pragma omp for
-        for (int i = 0; i < SIZE; i++) {
-            array[i] = rand() % 10000;  // Random number between 0 and 9999
+    // Fill the array with random integers between 1 and 10000
+    for (i = 0; i < SIZE; i++) {
+        arr[i] = rand() % 10000 + 1;
+    }
+
+    // Initialize the max_value variable
+    max_value = arr[0];
+
+    // Use OpenMP to parallelize the search for the maximum value
+    #pragma omp parallel for reduction(max:max_value)
+    for (i = 1; i < SIZE; i++) {
+        if (arr[i] > max_value) {
+            max_value = arr[i];
         }
     }
 
-    // Set initial maximum to the first element of the array
-    max_val = array[0];
-
-    // Parallel region to find the maximum value using OpenMP
-    #pragma omp parallel for reduction(max:max_val)
-    for (int i = 0; i < SIZE; i++) {
-        if (array[i] > max_val) {
-            max_val = array[i];
-        }
-    }
-
-    // Print the maximum value
-    printf("The maximum value in the array is: %d\n", max_val);
+    // Print the maximum value found
+    printf("Maximum value in the array: %d\n", max_value);
 
     return 0;
 }
